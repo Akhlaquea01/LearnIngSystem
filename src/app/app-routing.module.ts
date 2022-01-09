5d378d30f45a8bc836ff4d8251a8206f65728b0d
Akhlaque <akhlaquea01@gmail.com>
2022-01-09 19:04:43 +0530

    Updated Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginRegisterComponent } from './login-register/login-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login-register',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login-register',
+    component: LoginRegisterComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
