6f996b1e23972511615eae71f31f2d15e78088da
Akhlaque <akhlaquea01@gmail.com>
2022-01-14 19:21:53 +0530

    Retriving Local Storage

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -8,10 +8,15 @@ import { FirebaseService } from '../services/firebase.service';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
+  userName: any;
 
   constructor(private _firebaseService: FirebaseService, private _router: Router) { }
 
   ngOnInit(): void {
+    var User = localStorage.getItem('user');
+    this.userName=User!==null?JSON.parse(User).providerData[0].email:"User Name";
+    console.log(this.userName);
+    
   }
  logout() {
    this._firebaseService.logout();
